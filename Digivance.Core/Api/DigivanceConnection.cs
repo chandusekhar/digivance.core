36a01698f966e586354a36ef46e1877bd5fb4bc2
Dan Mayor <dmayor@digivance.com>
2020-01-11 12:38:14 -0500

    Adding Api.DigivanceConnection (and testing slack integration)

---
+++ Digivance.Core/Api/DigivanceConnection.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digivance.Core.Api {
+    public class DigivanceConnection {
+        public static string BaseUrl = "https://admin.digivance.com/api/beta";
+    }
+}
